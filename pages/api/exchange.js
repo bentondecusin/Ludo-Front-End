ee228771163685b1786dc9f5b09604c432f130a4
Benton Li <cl2597@cornell.edu>
2021-05-23 13:46:03 +0800

    configure exchange.js

--- pages/api/exchange.js
+++ pages/api/exchange.js
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function handler(req, res) {
-  console.log("roger")
-  fetch('http://0.0.0.0:2333').then(res => res.json())
+  var cmd = req.headers.cmd;
+  var reqOpt = {
+    method : 'POST',
+    body : cmd
+  }
+  fetch('http://0.0.0.0:2333', reqOpt).then(res => res.json())
   .then(data => res.status(200).json(data));
+  console.log("Exchange.js rogers");
+
 }
