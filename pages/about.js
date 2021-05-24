0dfe96d55450f968d5900d8c1daa1cd2f2554c92
Benton Li <cl2597@cornell.edu>
2021-05-24 20:40:30 +0800

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -11,8 +11,25 @@ export default function Home() {
       <Header></Header>
       <main className={styles.main__light}>
         <h1>About this game</h1>
-        <h3>Back-end: OCaml with cohttp</h3>
-        <h3>Front-end: Next.js & Node.js</h3>
+        <h3>
+          Back-end: OCaml with cohttp (
+          <a
+            class={styles.ref}
+            href="https://github.coecis.cornell.edu/cl2597/CS-3110-Final-Project/"
+          >
+            source code
+          </a>
+          )(NetID needed)
+        </h3>
+        <h3>
+          Front-end: Next.js & Node.js{" "}
+          <a
+            class={styles.ref}
+            href="https://github.com/bentondecusin/Ludo-Front-End"
+          >
+            (source code)
+          </a>
+        </h3>
       </main>
     </div>
   );
