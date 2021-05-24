1aee16c58cfbea3b7a6c4422bbf0c2b7d829d906
Benton Li <cl2597@cornell.edu>
2021-05-24 19:20:25 +0800

    Update gameboard.js

--- pages/api/gameboard.js
+++ pages/api/gameboard.js
@@ -229,7 +229,7 @@ export default function GameBoard(props) {
   const [dice, setDice] = useState(420);
 
   if (!started)
-    fetch("http://localhost:3000/api/exchange", startReq)
+    fetch("../api/exchange", startReq)
       .then(async (res) => res.json())
       .then((json) => {
         getStarted(true);
